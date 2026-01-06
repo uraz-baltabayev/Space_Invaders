c92d9d01e14ae5e46dd11e4b3a7b4d6c25a8e953
GM <GM@MacBook-Air.lan>
2026-01-06 13:40:55 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <raylib.h>
+
+int main()
+{
+    Color grey = {29, 29, 27, 255};
+    int windowWidth = 750;    
+    int windowHeight = 700;
+
+    InitWindow(windowWidth, windowHeight, "C++ Space Invaders");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(grey);
+        EndDrawing(); 
+    }
+    
+    
+    CloseWindow();
+}
